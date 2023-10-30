a6e2ae03a3f2bae1894347039a59329f8a667070
Nick Sette <njs2394@gmail.com>
2023-10-30 17:32:41 -0400

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  let response = ''
+
+  if (ride < 400) {
+    response = 'This one is on me!';
+  }
+  else if (ride > 400 && ride < 2000) {
+    response = 'That will be twenty bucks.'
+  }
+  else if (ride > 2000 && ride < 2500) {
+    response = 'I will gladly take your thirty bucks.'
+  }
+  else if (ride > 2500) {
+    response = 'No can do.'
+  }
+  console.log(response)
+  return (response)
+
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+
+  const response = city == 'NYC' ? "Ok, sounds good." : "No go.";
+  return (response)
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let response = ''
+
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      return (response)
+    case 'not as generous':
+      response = "Thank you.";
+      return (response)
+    default:
+      response = "Bye.";
+      return (response)
+  }
+
 }
+
